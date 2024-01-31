46d7db8cd4604490414e9bf8ff006013b9c59512
Yahalom8848 <yahalom8848@gmail.com>
2024-01-31 04:42:49 +0200

    feat: add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Tokyo Night'
+config.window_background_opacity = 0.9
+
+config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font_size = 10
+
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 100
+
+return config
