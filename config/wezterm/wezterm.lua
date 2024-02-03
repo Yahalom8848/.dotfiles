0d29a6ac6e8311c2c79696540a6fa7bc749a9979
Yahalom8848 <yahalom8848@gmail.com>
2024-02-03 03:31:11 +0200

    feat: remove window padding in wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ end
 
 config.color_scheme = 'Tokyo Night'
 config.window_background_opacity = 0.9
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 10
