99b796ecb566b2a4ffb19c042d7995de61e0a5de
Yahalom8848 <yahalom8848@gmail.com>
2024-02-06 21:47:42 +0200

    refactor: reorder weztem config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,19 +1,22 @@
 local wezterm = require 'wezterm'
 
-local config = {}
-
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
-config.color_scheme = 'Tokyo Night'
-config.window_background_opacity = 0.9
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-
-config.font = wezterm.font('JetBrainsMono Nerd Font')
-config.font_size = 10
-
-config.hide_tab_bar_if_only_one_tab = true
-config.initial_cols = 100
-
-return config
+return {
+    -- ===================================================================
+    -- * Theme
+    -- ===================================================================
+    color_scheme = 'Tokyo Night',
+    window_background_opacity = 0.9,
+    
+    -- ===================================================================
+    -- * Window
+    -- ===================================================================
+    hide_tab_bar_if_only_one_tab = true,
+    window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+    initial_cols = 100,
+    
+    -- ===================================================================
+    -- * Font
+    -- ===================================================================
+    font = wezterm.font('JetBrainsMono Nerd Font'),
+    font_size = 10,
+}
