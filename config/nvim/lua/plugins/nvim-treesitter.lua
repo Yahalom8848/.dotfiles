8bc3198605748dc9d82562816b4c31fdbce3497d
Yahalom8848 <yahalom8848@gmail.com>
2024-05-18 19:51:20 +0300

    fix: nvim treesitter remove temporary ignore_install option

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,6 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
-			ignore_install = { "gdshader" }, -- at the time of adding this treesitter is`nt able to install "gdshader"
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
