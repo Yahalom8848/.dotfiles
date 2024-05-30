63c6510f1bc535ad8d43aeda9bb76166d1dbec42
Yahalom8848 <yahalom8848@gmail.com>
2024-05-30 04:13:00 +0300

    style: format wezterm config file

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,25 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-    -- ===================================================================
-    -- * Theme
-    -- ===================================================================
+	-- ===================================================================
+	-- * Theme
+	-- ===================================================================
 
-    color_scheme = 'Tokyo Night',
-    window_background_opacity = 0.9,
-    
-    -- ===================================================================
-    -- * Window
-    -- ===================================================================
+	color_scheme = "Tokyo Night",
+	window_background_opacity = 0.9,
 
-    hide_tab_bar_if_only_one_tab = true,
-    window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-    initial_cols = 100,
-    
-    -- ===================================================================
-    -- * Font
-    -- ===================================================================
-    
-    font = wezterm.font('JetBrainsMono Nerd Font'),
-    font_size = 10,
+	hide_tab_bar_if_only_one_tab = true,
+	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+	initial_cols = 100,
+
+	-- ===================================================================
+	-- * Font
+	-- ===================================================================
+
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 10,
 }
