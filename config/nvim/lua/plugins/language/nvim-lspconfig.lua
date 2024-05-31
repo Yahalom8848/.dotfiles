b9d4a83035935d908136239be13826cda7bb7747
Yahalom8848 <yahalom8848@gmail.com>
2024-05-31 05:14:29 +0300

    fix: wait before hover

--- config/nvim/lua/plugins/language/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/language/nvim-lspconfig.lua
@@ -11,28 +11,29 @@ return {
 		local lspconfig = require("lspconfig")
 
 		local on_attach = function(_, bufnr)
-			local buff_keymap = function(mode, keys, cmd, desc, remap)
-				vim.keymap.set(mode, keys, cmd, { buffer = bufnr, desc = desc, remap = remap })
+			local buff_keymap = function(mode, keys, cmd, opts)
+				opts.buffer = bufnr
+				vim.keymap.set(mode, keys, cmd, opts)
 			end
 
-			buff_keymap("n", "<leader>df", vim.diagnostic.open_float, "[d]iagnostics [f]loting window")
-			buff_keymap("n", "<leader>dl", vim.diagnostic.setloclist, "[d]iagnostics [l]ist")
-			buff_keymap("n", "<leader>dp", vim.diagnostic.goto_prev, "[d]iagnostic [p]revious")
-			buff_keymap("n", "<leader>dn", vim.diagnostic.goto_next, "[d]iagnostic [n]ext")
+			buff_keymap("n", "<leader>df", vim.diagnostic.open_float, { desc = "[d]iagnostics [f]loting window" })
+			buff_keymap("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "[d]iagnostics [l]ist" })
+			buff_keymap("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "[d]iagnostic [p]revious" })
+			buff_keymap("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "[d]iagnostic [n]ext" })
 
-			buff_keymap("n", "<leader>gd", vim.lsp.buf.declaration, "[g]o to [d]eclaration")
-			buff_keymap("n", "<leader>gD", vim.lsp.buf.definition, "[g]o to [D]efinition")
-			buff_keymap("n", "<leader>gt", vim.lsp.buf.type_definition, "[g]o to [t]ype definition")
-			buff_keymap("n", "<leader>gi", vim.lsp.buf.implementation, "[g]o to [i]mplementation")
-			buff_keymap("n", "<leader>gr", vim.lsp.buf.references, "[g]o to [r]eference")
+			buff_keymap("n", "<leader>gd", vim.lsp.buf.declaration, { desc = "[g]o to [d]eclaration" })
+			buff_keymap("n", "<leader>gD", vim.lsp.buf.definition, { desc = "[g]o to [D]efinition" })
+			buff_keymap("n", "<leader>gt", vim.lsp.buf.type_definition, { desc = "[g]o to [t]ype definition" })
+			buff_keymap("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "[g]o to [i]mplementation" })
+			buff_keymap("n", "<leader>gr", vim.lsp.buf.references, { desc = "[g]o to [r]eference" })
 
-			buff_keymap("n", "<leader>h", vim.lsp.buf.hover, "[h]over")
+			buff_keymap("n", "<leader>h", vim.lsp.buf.hover, { desc = "[h]over", nowait = true })
 
-			buff_keymap("n", "<leader>cr", vim.lsp.buf.rename, "[c]ode [r]ename")
-			buff_keymap("n", "<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
+			buff_keymap("n", "<leader>cr", vim.lsp.buf.rename, { desc = "[c]ode [r]ename" })
+			buff_keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[c]ode [a]ction" })
 
-			buff_keymap({ "n", "v" }, "<leader>cc", "gc", "[c]ode [c]omment", true)
-			buff_keymap({ "n", "v" }, "<leader>ccl", "gcc", "[c]ode [c]omment [l]ine", true)
+			buff_keymap({ "n", "v" }, "<leader>cc", "gc", { desc = "[c]ode [c]omment", remap = true })
+			buff_keymap({ "n", "v" }, "<leader>ccl", "gcc", { desc = "[c]ode [c]omment [l]ine", remap = true })
 
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				desc = "Format buffer before write",
