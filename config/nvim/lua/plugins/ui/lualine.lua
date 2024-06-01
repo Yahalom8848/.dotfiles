09d430b5e7d4b6e394abc7f788963974034def6c
Yahalom8848 <yahalom8848@gmail.com>
2024-06-01 06:05:55 +0300

    feat: nvim update lualine theme

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -1,9 +1,23 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 	config = function()
 		require("lualine").setup({
-			theme = "tokyonight",
+			options = {
+				theme = "tokyonight",
+				component_separators = "|",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { { "mode", separator = { left = "" } } },
+				lualine_b = { "filename", "branch" },
+				lualine_c = { "diff", "diagnostics" },
+				lualine_x = { "filesize", "encoding", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { { "location", separator = { right = " " } } },
+			},
 		})
 	end,
 }
