4b04904107e531988d558add681b629396191ccd
Yahalom8848 <yahalom8848@gmail.com>
2024-06-01 06:07:11 +0300

    feat: nvim add github copilot

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+		"AndreM222/copilot-lualine",
 	},
 	config = function()
 		require("lualine").setup({
@@ -14,7 +15,7 @@ return {
 				lualine_a = { { "mode", separator = { left = "" } } },
 				lualine_b = { "filename", "branch" },
 				lualine_c = { "diff", "diagnostics" },
-				lualine_x = { "filesize", "encoding", "filetype" },
+				lualine_x = { "copilot", "filesize", "encoding", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { { "location", separator = { right = " " } } },
 			},
