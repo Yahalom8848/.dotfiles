35f912d164410aa5062f35288da3dc52ecd5498c
Yahalom8848 <yahalom8848@gmail.com>
2024-12-24 18:03:05 +0200

    feat: nvim center screen when moving up or down

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "[l]azy" })
 
 vim.keymap.set("n", "<leader>s", vim.cmd.w, { desc = "[s]ave" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
